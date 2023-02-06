06a729d86301bbc73579ccb4dc807c05343bbad1
Boris Berak <bberak@gmail.com>
2023-02-06 23:06:24 +1100

    Small improvements

--- src/mmap-shared.c
+++ src/mmap-shared.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define  PAGESIZE   4096
+#define PAGESIZE 4096
 
 int main(int argc, char **argv) {
     int fd;
@@ -14,9 +14,9 @@ int main(int argc, char **argv) {
     char *ptr;
 
     // open a regular file, write a string, and map it into memory
-    fd = open(argv[1],O_RDWR|O_CREAT,S_IRWXU);
+    fd = open(argv[1], O_RDWR|O_CREAT, S_IRWXU);
     write(fd, string, sizeof(string) - 1);
-    ptr = (char*) mmap(NULL,PAGESIZE,PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
+    ptr = (char*) mmap(NULL, PAGESIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
     close(fd);
 
     printf("pointer to memory mapped file: %p \n", ptr);
